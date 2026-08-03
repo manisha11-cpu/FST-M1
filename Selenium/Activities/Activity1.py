fab001123eaa624f00be8f50aa165ec9bae9d74a
manisha11-cpu <Doddapaneni.Manisha@ibm.com>
2026-08-03 13:17:50 +0530

    Create Activity1.py

---
+++ Selenium/Activities/Activity1.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge()
+driver.get("https://training-support.net")
+
+print(driver.title)
+
+driver.find_element(By.LINK_TEXT, "About Us").click()
+print(driver.title)
+
+driver.quit()
